828aad752310ab36230d15586bcb92da91f76a03
Gabriel Lupu <lupugabriel1@gmail.com>
2017-06-27 10:07:55 +0300

    Travis-CI Debug - check local files

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,7 +1,7 @@
 ApiKey=$1
 Source=$2
 
-nuget pack ./MangoPay.SDK/MangoPay.SDK.csproj -Build
+nuget pack ./MangoPay.SDK/MangoPay.SDK.csproj -Build -Verbosity detailed
 # nuget pack ./MangoPay.SDK/MangoPay.SDK.nuspec -Verbosity detailed
 find . -type f -name "*.nupkg"
-nuget push ./DnDGen.MangoPay.SDK.*.nupkg -Verbosity detailed -ApiKey $ApiKey -Source $Source
+# nuget push ./DnDGen.MangoPay.SDK.*.nupkg -Verbosity detailed -ApiKey $ApiKey -Source $Source
