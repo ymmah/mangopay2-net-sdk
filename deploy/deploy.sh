30f7dd6ffba3b599c096d224dde424b135e77b9b
Gabriel Lupu <lupugabriel1@gmail.com>
2017-06-27 09:27:22 +0300

    Travis deploy script update

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -2,5 +2,5 @@ ApiKey=$1
 Source=$2
 
 nuget pack ./MangoPay.SDK/MangoPay.SDK.csproj -Build
-nuget pack ./MangoPay.SDK/MangoPay.SDK.nuspec -Verbosity detailed
+# nuget pack ./MangoPay.SDK/MangoPay.SDK.nuspec -Verbosity detailed
 nuget push ./DnDGen.MangoPay.SDK.*.nupkg -Verbosity detailed -ApiKey $ApiKey -Source $Source
